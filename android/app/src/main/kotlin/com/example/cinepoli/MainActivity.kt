504520082d4a853287dec3ec3d194bb9e42c82b2
victorhugonicolau <victor.castro29@etec.sp.gov.br>
2024-08-13 22:51:37 -0300

    API DO MAPA AQUI ---> Tue, Aug 13, 2024 10:51:37 PM

---
+++ android/app/src/main/kotlin/com/example/cinepoli/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cinepoli
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/cinema_page.dart
@@ -0,0 +1,63 @@
+import 'package:cinepoli/componentes/cinema_item.dart';
+import 'package:cinepoli/componentes/mapa.dart';
+import 'package:cinepoli/vm/cinema_vm.dart';
+import 'package:cinepoli/vm/filme_vm.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CinemaPage extends StatelessWidget {
+  const CinemaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final vm = Provider.of<CinemaViewModel>(context);
+    final cinemas = vm.cinemas;
+
+    final screenSize = MediaQuery.of(context).size;
+    final listHeight = screenSize.height * 0.4;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cinema"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Column(
+        children: [
+          const Expanded(
+            child: SizedBox(
+              width: double.infinity,
+              child: Mapa(),
+            ),
+          ),
+          SizedBox(
+            width: double.maxFinite,
+            height: listHeight,
+            child: ListView.builder(
+              itemCount: cinemas.length,
+              itemBuilder: (context, index) => GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () {
+                  final filmeVM = Provider.of<FilmeVIewModel>(
+                    context,
+                    listen: false,
+                  );
+
+                  filmeVM.useCinema(cinemas[index].id);
+
+                  Navigator.pushNamed(
+                    context,
+                    "/filme",
+                    arguments: cinemas[index].id,
+                  );
+                },
+                child: CinemaItem(
+                  cinema: cinemas[index],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
